a5e6a647a9490bf41187bb16d414fec70dd13433
robbydyer <robby.dyer@gmail.com>
2022-03-29 15:59:12 -0400

    wip

--- pkg/countdownboard/render.go
+++ pkg/countdownboard/render.go
@@ -3,12 +3,16 @@ package countdownboard
 import (
 	"context"
 	"fmt"
+	"image"
 	"image/color"
+	"image/draw"
 	"math"
 	"time"
 
 	"github.com/robbydyer/sports/pkg/board"
+	"github.com/robbydyer/sports/pkg/rgbmatrix-rpi"
 	"github.com/robbydyer/sports/pkg/rgbrender"
+	"go.uber.org/zap"
 )
 
 // ScrollRender ...
@@ -39,7 +43,6 @@ func (s *CountdownBoard) Render(ctx context.Context, canvas board.Canvas) error 
 	return nil
 }
 
-// Render ...
 func (s *CountdownBoard) render(ctx context.Context, canvas board.Canvas) (board.Canvas, error) {
 	s.boardCtx, s.boardCancel = context.WithCancel(ctx)
 	zeroed := rgbrender.ZeroedBounds(canvas.Bounds())
@@ -48,21 +51,78 @@ func (s *CountdownBoard) render(ctx context.Context, canvas board.Canvas) (board
 		return nil, err
 	}
 
-	for _, event := range s.config.Events {
-		daysLeft := int(math.Ceil(time.Until(event.date).Hours() / 24.0))
-		if err := writer.WriteAligned(
-			rgbrender.CenterCenter,
-			canvas,
-			zeroed,
-			[]string{
-				fmt.Sprintf("Days until %s", event.Title),
-				fmt.Sprintf("%d", daysLeft),
-			},
-			color.White,
-		); err != nil {
+	var scrollCanvas *rgbmatrix.ScrollCanvas
+	if canvas.Scrollable() && s.config.ScrollMode.Load() {
+		base, ok := canvas.(*rgbmatrix.ScrollCanvas)
+		if !ok {
+			return nil, fmt.Errorf("invalid scroll canvas")
+		}
+
+		var err error
+		scrollCanvas, err = rgbmatrix.NewScrollCanvas(base.Matrix, s.log)
+		if err != nil {
 			return nil, err
+		}
+		scrollCanvas.SetScrollSpeed(s.config.scrollDelay)
+		scrollCanvas.SetScrollDirection(rgbmatrix.RightToLeft)
+	}
+
+EVENTS:
+	for _, event := range s.config.Events {
+		select {
+		case <-s.boardCtx.Done():
+			return nil, context.Canceled
+		default:
+		}
+		if err := s.renderEvent(s.boardCtx, canvas, event, writer); err != nil {
+			continue EVENTS
+		}
+
+		if scrollCanvas != nil && s.config.ScrollMode.Load() {
+			scrollCanvas.AddCanvas(canvas)
+			draw.Draw(canvas, canvas.Bounds(), &image.Uniform{color.Black}, image.Point{}, draw.Over)
+			continue EVENTS
+		}
+
+		if err := canvas.Render(s.boardCtx); err != nil {
+			s.log.Error("failed to render countdown board",
+				zap.Error(err),
+			)
+			continue EVENTS
+		}
+
+		if !s.config.ScrollMode.Load() {
+			select {
+			case <-ctx.Done():
+				return nil, context.Canceled
+			case <-time.After(s.config.boardDelay):
+			}
 		}
 	}
 
+	if canvas.Scrollable() && scrollCanvas != nil {
+		scrollCanvas.Merge(s.config.TightScrollPadding)
+		return scrollCanvas, nil
+	}
+
 	return nil, nil
+}
+
+func (s *CountdownBoard) renderEvent(ctx context.Context, canvas board.Canvas, event *Event, writer *rgbrender.TextWriter) error {
+	zeroed := rgbrender.ZeroedBounds(canvas.Bounds())
+	daysLeft := int(math.Ceil(time.Until(event.date).Hours() / 24.0))
+	if err := writer.WriteAligned(
+		rgbrender.CenterCenter,
+		canvas,
+		zeroed,
+		[]string{
+			fmt.Sprintf("Days until %s", event.Title),
+			fmt.Sprintf("%d", daysLeft),
+		},
+		color.White,
+	); err != nil {
+		return err
+	}
+
+	return nil
 }
