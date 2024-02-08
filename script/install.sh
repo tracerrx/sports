f19a662b3a10c8f7dc0e6361b57a3af66d3e718e
Nicholas Saraniti <nick@nirica.com>
2024-02-07 20:37:19 -0500

    Update install.sh to use tracerrx github

--- script/install.sh
+++ script/install.sh
@@ -13,13 +13,13 @@ cd "${tmp}"
 case "${ARCH}" in
 aarch64|armv7l)
   echo "Installing sportsmatrix for ${ARCH}"
-  latesturl="https://api.github.com/repos/robbydyer/sports/releases/latest"
+  latesturl="https://api.github.com/repos/tracerrx/sports/releases/latest"
 
   curl -s "${latesturl}" | grep browser_download_url | grep deb | cut -d: -f2,3 | tr -d \" | wget -qi -
   ;;
 armv6l)
   echo "This architecture is deprecated! The last supported version is v0.0.83"
-  wget -q "https://github.com/robbydyer/sports/releases/download/v0.0.83/sportsmatrix-0.0.83_armv6l.deb"
+  wget -q "https://github.com/tracerrx/sports/releases/download/v0.0.83/sportsmatrix-0.0.83_armv6l.deb"
   ;;
 *)
   echo "Unsupported architecture '${ARCH}'"
